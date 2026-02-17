48a27f8c5ab91372eb11bf5dd42d7f32df715172
Liuisei <a0966891512@gmail.com>
2026-02-17 23:55:59 +0900

    fix: load roslyn xml from third-party resources

--- Packages/com.nodecodesync.unity/Editor/NodeCodeSync/Editor/ASTEditor/Schema/RoslynXmlLoader.cs
+++ Packages/com.nodecodesync.unity/Editor/NodeCodeSync/Editor/ASTEditor/Schema/RoslynXmlLoader.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using System.Xml.Linq;
 using UnityEngine;
 
@@ -7,16 +6,26 @@ namespace NodeCodeSync.Editor.ASTEditor
 {
     public static class RoslynXmlLoader
     {
+        private const string RoslynSyntaxResourcePath = "ThirdParty/Roslyn/RoslynSyntax";
         public static XDocument Load()
         {
-            var textAsset = Resources.Load<TextAsset>("RoslynSyntax");
-            if (textAsset == null)
+            try
             {
-                Debug.LogError("RoslynSyntax.xml not found in Resources");
+                var textAsset = Resources.Load<TextAsset>(RoslynSyntaxResourcePath);
+                if (textAsset == null)
+                {
+                    Debug.LogError($"RoslynSyntax.xml not found at Resources/{RoslynSyntaxResourcePath}.xml");
+                    return null;
+                }
+
+                return XDocument.Parse(textAsset.text);
+            }
+            catch (System.Exception ex)
+            {
+                Debug.LogError("Failed to load or parse RoslynSyntax.xml.");
+                Debug.LogException(ex);
                 return null;
             }
-
-            return XDocument.Parse(textAsset.text);
         }
     }
 }
